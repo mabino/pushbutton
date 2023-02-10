cbe8104c142ceee7bb45714eed443d87ac4f45f6
mabino <mabino@gmail.com>
2023-02-10 15:34:47 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/* Place outside the webroot and replace app-token and user-key with values from Pushover */
+$app_token = "app-token";
+$user_key = "user-key";
+
+return array(
+  'token' => $app_token,
+  'user_key' => $user_key,
+);
+
+?>
